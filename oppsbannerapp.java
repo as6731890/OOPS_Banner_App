09b8df91e0669f50880627cab17811228b21ebef
as6731890 <as6731@srmist.edu.in>
2026-03-03 00:34:34 +0530

    Update oppsbannerapp.java

--- oppsbannerapp.java
+++ oppsbannerapp.java
@@ -1,1 +1,31 @@
+/*
+ * OOPSBannerApp UC3 - OOPS Banner Application (Use Case 3)
+ * Uses String.join() for better memory efficiency
+ */
 
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " *** ", " *** ", " **** ", " *****"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*   *", "*"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*   *", "*"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "**** ", " *****"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*    ", "     *"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*    ", "     *"));
+
+        System.out.println(String.join(" ",
+                " *** ", " *** ", "*    ", " *****"));
+    }
+}
